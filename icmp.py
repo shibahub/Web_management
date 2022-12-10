f74c90501956481f15879955e39eb659e95ceaee
shibahub <78590473+shibahub@users.noreply.github.com>
2022-12-10 16:04:30 +0700

    Add files via upload

---
+++ icmp.py
@@ -0,0 +1,32 @@
+from pysnmp.hlapi import *
+
+def icmp_value(ip):
+    result=[]
+    print(f'icmp request IP: {ip}')
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        getCmd(SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget((ip, 161)),
+            ContextData(),
+            #icmpInEchos
+            ObjectType(ObjectIdentity('1.3.6.1.2.1.5.8.0')),
+            #icmpInEchosReq
+            ObjectType(ObjectIdentity('1.3.6.1.2.1.5.9.0')),
+            #icmpOutEchos
+            ObjectType(ObjectIdentity('1.3.6.1.2.1.5.21.0')),
+            #icmpOutEchosReps
+            ObjectType(ObjectIdentity('1.3.6.1.2.1.5.22.0')))
+    )
+
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+    else:
+        for varBind in varBinds:
+            #print(' = '.join([x.prettyPrint() for x in varBind]))
+            #print(str(varBind))
+
+            result.append(str(varBind))
+    return result
---
+++ system.py
@@ -0,0 +1,21 @@
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+def system_value(ip):
+    result=[]
+    errorIndication, errorStatus, errorIndex, varBindTable = cmdgen.CommandGenerator().nextCmd(
+        cmdgen.CommunityData('test-agent', 'public'),
+        cmdgen.UdpTransportTarget((ip, 161)),
+        (1,3,6,1,2,1,1)
+        )
+
+    if errorIndication:
+        print('1')
+    else:
+        if errorStatus:
+            print ("2")
+        else:
+            for varBindTableRow in varBindTable:
+                for name, val in varBindTableRow:
+                    tmp = name.prettyPrint()+' = '+val.prettyPrint()
+                    #print(tmp)
+                    result.append(tmp)
+    return result
