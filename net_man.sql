50f899319aab4d05772dc2e6ad0bbf6433c1a694
shibahub <78590473+shibahub@users.noreply.github.com>
2022-12-26 09:56:44 +0700

    database

--- net_man.sql
+++ net_man.sql
@@ -1,26 +1,26 @@
 create database net_man;
 use net_man;
 Create table Icmp(
-    ip varchar(255), 
+	ip varchar(255), 
     icmp_in varchar(255), 
     icmp_in_rep varchar(255), 
     icmp_out varchar(255), 
     icmp_out_rep varchar(255), 
-    times varchar(255),
-    PRIMARY KEY (ip)
+    times varchar(255)
+    
 );
 Create table System_info(
-    ip varchar(255), 
-    OID varchar(255), 
-    INFO varchar(255), 
-    times varchar(255),
-    PRIMARY KEY (ip)
+	ip varchar(255), 
+	OID varchar(255), 
+	INFO varchar(255), 
+	times varchar(255)
+    
 );
 Create table IP_table(
-    ip varchar(255), 
-    nexthop varchar(255), 
-    destination varchar(255),
-    subnet  varchar(255),
-    times varchar(255),
-    PRIMARY KEY (ip)
+	ip varchar(255), 
+	nexthop varchar(255), 
+	destination varchar(255),
+	subnet  varchar(255),
+	times varchar(255)
+   
 );
